dd5e6590c7854012fe4df3a3875e752cc330bea0
Kristjan Liiva <krisliiv@greeny.cs.tlu.ee>
2017-04-20 09:11:29 +0300

    KT 1.punkt tehtud

---
+++ Proov1.java
@@ -0,0 +1,22 @@
+public class Proov1{
+	
+	public static void main(String[] arg){
+		
+		Toiduaine t1 = new Toiduaine("Kartul", 8.0, 1.0, 91.0);
+		Toiduaine t2 = new Toiduaine("Hapukoor", 2.7, 20.0, 4.0);
+		
+		Toidukomponent k1 = new Toidukomponent();
+		k1.t = t1;
+		k1.kogus = 100.0;	//Grammides
+		
+		Toidukomponent k2 = new Toidukomponent();
+		k2.t = t2;
+		k2.kogus = 30.0;    //Grammides
+		
+		System.out.println(k1.rasvakogus());
+		System.out.println(k2.rasvakogus());
+		
+	}
+	
+	
+}
