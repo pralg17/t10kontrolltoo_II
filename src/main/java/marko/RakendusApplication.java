c04ff380968e22f3d98bbcd65a4b0266f2e14422
MK <mrxkollo@users.noreply.github.com>
2017-04-20 14:26:51 +0300

    Initial files

---
+++ src/main/java/marko/RakendusApplication.java
@@ -0,0 +1,12 @@
+package marko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakendusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakendusApplication.class, args);
+	}
+}
