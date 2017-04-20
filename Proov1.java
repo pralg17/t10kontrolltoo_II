9b8679056a1276606475f6368f123be1f87b4687
henrivajak <henrivajak@users.noreply.github.com>
2017-04-20 09:38:46 +0300

    Add files via upload

---
+++ Proov1.java
@@ -0,0 +1,8 @@
+public class Proov1{
+	public static void main(String[] arg){
+		Toiduained t1=new Toiduained("Kartul", 5, 10, 15);
+		Joogipudel p1=new Joogipudel();
+		
+		System.out.println(Toiduained());
+	}
+}
