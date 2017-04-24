1804d08fc0607493833a04d91109289449fd815e
karojyrg <karojyrg@greeny.cs.tlu.ee>
2017-04-24 12:18:35 +0300

    esimene ul valmis

--- Proov1.java
+++ Proov1.java
@@ -10,19 +10,38 @@ public class Proov1{
 		Toidukomponendid k1 = new Toidukomponendid();
 		k1.t = t1;
 		k1.kogus = 100;
-		
+
 		Toidukomponendid k2 = new Toidukomponendid();
 		k2.t = t2;
-		k1.kogus = 30;
-		
+		k2.kogus = 30;
+
 		Toidukomponendid k3 = new Toidukomponendid();
 		k3.t = t3;
 		k3.kogus = 50;
-		
-		System.out.println(k1.kogus);
-		System.out.println(k2.kogus);
-		System.out.println(k3.kogus);
 
-		
+		System.out.println("Kartuli kogus grammides on: "+(k1.kogus));
+		System.out.println("Hapukoore kogus grammides on: "+(k2.kogus));
+		System.out.println("Vorsti kogus grammides on: "+(k3.kogus));
+
+    System.out.println();
+
+    System.out.println("Rasvakogus kartulis on: "+(k1.rasvakogus()));
+    System.out.println("Rasvakogus hapukoores on: "+(k2.rasvakogus()));
+    System.out.println("Rasvakogus vorstis on: "+(k3.rasvakogus()));
+
+
+
     }
 }
+
+/* Tulemus:
+
+Kartuli kogus grammides on: 100.0
+Hapukoore kogus grammides on: 30.0
+Vorsti kogus grammides on: 50.0
+
+Rasvakogus kartulis on: 0.0
+Rasvakogus hapukoores on: 6.0
+Rasvakogus vorstis on: 9.5
+
+*/
--- Toiduained.java
+++ Toiduained.java
@@ -3,18 +3,18 @@ public class Toiduained{
 	double valgud;
 	double rasvad;
 	double susivesikud;
-	
+
 	public Toiduained(String nimetus, double valgud, double rasvad, double susivesikud){
 		if(nimetus==null || nimetus.length()==0){
             throw new RuntimeException("Nimetus puudub");
         }
-		
+
 		this.nimetus = nimetus;
 		this.valgud = valgud;
 		this.rasvad = rasvad;
 		this.susivesikud = susivesikud;
-		
-		if(valgud + rasvad + susivesikud >=100){
+
+		if(valgud + rasvad + susivesikud >100){
 			throw new RuntimeException("Protsent kokku ei saa uletada 100!");
 		}
 	}
