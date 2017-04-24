3378cd7cacac29591769fa7eb39553c724704ff9
karojyrg <karojyrg@greeny.cs.tlu.ee>
2017-04-24 13:41:15 +0300

    teine ul poolik

---
+++ src/main/java/karolmilaine/Rakendus.java
@@ -0,0 +1,90 @@
+package karolmilaine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+public class Rakendus{
+	private final ToiduainedDao toiduainedDao;
+
+    @Autowired
+    public Rakendus(ToiduainedDao toiduainedDao) {
+        this.toiduainedDao = toiduainedDao;
+    }
+	
+	@RequestMapping("/listall")
+    String listall(String nimetus) {
+        StringBuffer sb = new StringBuffer();
+        for (Toiduained item : toiduainedDao.findAll()) {
+            sb.append(item);
+        }
+        String thead = "<tr><th>Id</th><th>Nimetus</th><th>Valgud</th><th>Rasvad</th><th>Susivesikud</th></tr>";
+        return thead + sb.toString();
+    }
+
+    @RequestMapping("/lisa")
+    String lisa(String nimetus, Integer valgud, Integer rasvad, Integer susivesikud) {
+        Toiduained toiduained = new Toiduained();
+        toiduained.nimetus = nimetus;
+		toiduained.valgud = valgud;
+		toiduained.rasvad = rasvad;
+		toiduained.susivesikud = susivesikud;
+        
+        toiduainedDao.save(toiduained);
+        return "Lisatud " + nimetus + valgud + rasvad + susivesikud;
+    }
+
+    @RequestMapping("/kustuta")
+    String kustuta(Integer id) {
+        Toiduained toiduained = toiduainedDao.findOne(id);
+        toiduainedDao.delete(toiduained);
+        return toiduained.nimetus + " " + toiduained.valgud +  " " + toiduained.rasvad + " " + toiduained.susivesikud + " on kustutatud";
+    }
+
+    public static void main(String[] args) {
+        System.getProperties().put("server.port", 1234);
+        SpringApplication.run(Rakendus.class, args);
+    }
+	
+    /*public static void main(String[] arg){
+        Toiduained t1= new Toiduained("kartul", 1.71, 0, 20.1);
+        Toiduained t2= new Toiduained("hapukoor", 2.8, 20, 3.2);
+        Toiduained t3= new Toiduained("vorst", 12, 19, 4);
+
+		Toidukomponendid k1 = new Toidukomponendid();
+		k1.t = t1;
+		k1.kogus = 100;
+
+		Toidukomponendid k2 = new Toidukomponendid();
+		k2.t = t2;
+		k2.kogus = 30;
+
+		Toidukomponendid k3 = new Toidukomponendid();
+		k3.t = t3;
+		k3.kogus = 50;
+
+		System.out.println("Kartuli kogus grammides on: "+(k1.kogus));
+		System.out.println("Hapukoore kogus grammides on: "+(k2.kogus));
+		System.out.println("Vorsti kogus grammides on: "+(k3.kogus));
+
+    System.out.println();
+
+    System.out.println("Rasvakogus kartulis on: "+(k1.rasvakogus()));
+    System.out.println("Rasvakogus hapukoores on: "+(k2.rasvakogus()));
+    System.out.println("Rasvakogus vorstis on: "+(k3.rasvakogus()));
+
+
+
+    }*/
+	
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/rakendus-1.jar
+//http://greeny.cs.tlu.ee:1234/
+}
+
---
+++ src/main/java/karolmilaine/Toiduained.java
@@ -0,0 +1,40 @@
+package karolmilaine;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "toiduained")
+public class Toiduained{
+	@Id
+    @GeneratedValue
+	Integer id;
+	String nimetus;
+	Integer valgud;
+	Integer rasvad;
+	Integer susivesikud;
+	
+	@Override
+    public String toString() {
+        return "<tr><td>" + id + "</td><td>" + nimetus + "</td><td>" + valgud + "</td><td>" + rasvad +
+		"</td><td>" + susivesikud + "</td><td>";
+    }
+
+	/*public Toiduained(String nimetus, double valgud, double rasvad, double susivesikud){
+		if(nimetus==null || nimetus.length()==0){
+            throw new RuntimeException("Nimetus puudub");
+        }
+
+		this.nimetus = nimetus;
+		this.valgud = valgud;
+		this.rasvad = rasvad;
+		this.susivesikud = susivesikud;
+
+		if(valgud + rasvad + susivesikud >100){
+			throw new RuntimeException("Protsent kokku ei saa uletada 100!");
+		}
+	} */
+}
