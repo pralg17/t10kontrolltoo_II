f5cb0c28960de92b5eb1cc0caaa4a21572e41e2f
hensav <hensav@gmail.com>
2017-04-27 09:54:10 +0300

    Kontrolltöö 2. commit

--- src/main/java/hello/Rakendus.java
+++ src/main/java/hello/Rakendus.java
@@ -16,10 +16,28 @@ public class Rakendus {
         this.toitDao = toitDao;
     }
 
+    //Loo mõned toidukomponendid (nt. 100 g kartuleid, 30 g hapukoort, 50 g vorsti).
+    @RequestMapping("/toidukomponendid")
+    public String toiduKomponent(){
+        //Loo mõni toiduaine (nt. kartul, hapukoor, vorst)
+        Toit kartul = new Toit(1, "kartul", 1, 1, 1);
+        Toit hapukoor = new Toit(2,"hapukoor", 2, 2, 2);
+        Toit vorst = new Toit(3,"vorst", 3, 3, 3);
 
+        //Loo mõned toidukomponendid (nt. 100 g kartuleid, 30 g hapukoort, 50 g vorsti).
+
+        Toidukomponent kartulKokku = new Toidukomponent();
+        kartulKokku.toit = kartul;
+        kartulKokku.kogus = 10;
+
+        //Lisa toidukomponendile käsklus selle sees leiduva rasvakoguse arvutamiseks.
+
+        return null;
+    }
 
     @RequestMapping("/listall")
-     String listall(String mark) {
+     String listall() {
+
         StringBuffer sb = new StringBuffer();
         for (Toit item : toitDao.findAll()) {
             sb.append(item);
@@ -28,8 +46,21 @@ public class Rakendus {
         return thead + sb.toString();
     }
 
+    @RequestMapping("/otsi") //rasva % järgi
+    public String vahemik(double rasvadOtsitav){
+
+        StringBuffer sb = new StringBuffer();
+        for(Toit item : toitDao.findAll()){
+            if (Math.abs(rasvadOtsitav-item.rasvad) <= 10){
+                sb.append(item);
+            }
+        }
+        String thead = "<tr><th>Id</th><th>Nimetus</th><th>Valgud</th><th>Rasvad</th><th>Sysivesikud</th></tr>";
+        return thead + sb.toString();
+    }
+
     @RequestMapping("/lisa")
-    String lisa(String nimetus, Integer valgud, Integer rasvad, Integer sysivesikud) {
+    String lisa(Integer id, String nimetus, Integer valgud, Integer rasvad, Integer sysivesikud) {
         if(valgud>100){
             throw new RuntimeException("100");
         }
@@ -39,10 +70,15 @@ public class Rakendus {
         toit.rasvad = rasvad;
         toit.sysivesikud = sysivesikud;
         toitDao.save(toit);
-        return "Lisatud " + nimetus + valgud + rasvad + sysivesikud;
+        return nimetus + "on lisatud , kuvamiseks vajuta nuppu";
     }
 
-
+    @RequestMapping("/kustuta")
+    public String kustuta(Integer id) {
+        Toit toit = toitDao.findOne(id);
+        toitDao.delete(toit);
+        return "Kustutasid toiduaine" + toit.nimetus;
+    }
 
     public static void main(String[] args) {
         System.getProperties().put("server.port", 4207);
--- src/main/java/hello/Toit.java
+++ src/main/java/hello/Toit.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "toiduained") //oli user
+@Table(name = "toiduained")
 public class Toit {
 	@Id
 	@GeneratedValue
@@ -16,7 +16,20 @@ public class Toit {
 	Integer rasvad;
 	Integer sysivesikud;
 
-	@Override
+	public Toit() {
+	//tühi konstruktor et saaks veebilehelt ligi
+	}
+
+	public Toit(Integer id, String nimetus, Integer valgud, Integer rasvad, Integer sysivesikud) {
+		this.id = id;
+		this.nimetus = nimetus;
+		this.valgud = valgud;
+		this.rasvad = rasvad;
+		this.sysivesikud = sysivesikud;
+	}
+
+
+		@Override
 	public String toString() {
 		return "<tr><td>" + id + "</td><td>" + nimetus + "</td><td>" + valgud + "</td><td>" + rasvad + "</td><td>"+ sysivesikud;
 	}
