33c47e5e8f76d52d388265c25f5723cdaa70fe99
Mihkel Põder <mpoder@users.noreply.github.com>
2017-04-27 09:29:58 +0300

    Viimase kontrolltöö tunni tulemus.

--- main.js
+++ main.js
@@ -17,19 +17,23 @@ class Kolmkõla {
     return this.maintone + " " + this.sterts + " " + this.kvint;
   }
 
-  test() {
-    // automaattesti jaoks cuz i'm lazy
-  }
+}
+
+function showTriad(basenote) {
+
 }
 
 function getAlphabetic(note) {
-  if (typeof note == "number") {
+  if (!isNaN(note)) {
+  note = Number(note);
+  if (note >= 73) {note -= 12;}
+  if (note <= 60) {note += 12;}
   var heights = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72];
   var names = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "B", "H", "C"];
   var noteHeight = names[heights.indexOf(note)];
   return noteHeight;
 } else {
-  return note;
+  return Number(note);
 }
 }
 
@@ -38,9 +42,9 @@ function getNumeric(note) {
 {  var heights = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72];
   var names = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "B", "H", "C"];
   var noteHeight = heights[names.indexOf(note)];
-  return noteHeight;
+  return Number(noteHeight);
 } else {
-    return note;
+  return Number(note);
   }
 }
 
@@ -56,10 +60,29 @@ class Lugu {
   addChord() {
     var chordBase = document.getElementById("songAddChord").value;
     chordBase = getNumeric(chordBase);
-    console.log(chordBase);
     this.taktid.push(new Takt(chordBase));
     this.printSong();
   }
+
+  showChordConstructs(option) {
+    var outputLocation = document.getElementById("songoutput");
+    var output = "";
+    var i;
+    for (i = 0; i < this.taktid.length; i++) {
+      var mt = Number(this.taktid[i].chord.maintone);
+    if (option == "notes") {
+      output += "" + getAlphabetic(mt) + " " + getAlphabetic(mt+4) + " " + getAlphabetic(mt+7) + "<br>";
+    } else {
+      var terts = Number(mt) + 4;
+      var kvint = Number(mt) + 7;
+      if (terts >= 73) {terts -= 12;}
+      if (kvint >= 73) {kvint -= 12;}
+      output += "" + mt + " " + terts + " " + kvint + "<br>";
+    }
+    }
+    outputLocation.innerHTML = output;
+  }
+
   printSong() {
     var outputLocation = document.getElementById("songoutput");
     var output = "";
@@ -90,12 +113,16 @@ function getPlayedNotes() {
   var value = textbox.value;
   var heights = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72];
   var names = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "B", "H", "C"];
-  var noteHeight = heights[names.indexOf(value)];
+  if (isNaN(value))
+  {var noteHeight = heights[names.indexOf(value)];} else {
+    noteHeight = Number(value);
+  }
+  //console.log(noteHeight);
   temp = new Kolmkõla(noteHeight);
   output.innerHTML = temp.getNames();
 }
 
-function test() {
+function testFirst() {
   var expectedOutput1 = "C E G";
   var expectedOutput2 = "A C# E";
   var expectedOutput3 = "C E G";
@@ -119,7 +146,35 @@ function test() {
   }
 }
 
+function testThird() {
+  var textBox = document.getElementById("songAddChord");
+  var songoutput = document.getElementById("songoutput");
+  var successfulTests = 0
+  textBox.value = "C";
+  Song.addChord();
+  textBox.value = "E";
+  Song.addChord();
+  textBox.value = "67";
+  Song.addChord();
+  textBox.value = "H";
+  Song.addChord();
+  textBox.value = "B";
+  Song.addChord();
+  console.log(songoutput.innerHTML);
+  if (songoutput.innerHTML == "CEGHB") {successfulTests += 1;}
+  Song.removeLastChord();
+  console.log(songoutput.innerHTML);
+  if (songoutput.innerHTML == "CEGH") {successfulTests += 1;}
+  Song.showChordConstructs('numbers');
+  console.log(songoutput.innerHTML);
+  if (songoutput.innerHTML == "60 64 67<br>64 68 71<br>67 71 62<br>71 63 66<br>") {successfulTests += 1;}
+  Song.showChordConstructs('notes');
+  if (songoutput.innerHTML == "C E G<br>E G# H<br>G H D<br>H Eb F#<br>") {successfulTests += 1;}
+  console.log(songoutput.innerHTML);
+  console.log("Läbiti " + successfulTests + " testi 4st.");
+}
+
 CKolmkõla = new Kolmkõla(60);
 FKolmkõla = new Kolmkõla(65);
 GKolmkõla = new Kolmkõla(67);
-test();
+testFirst();
